d7ce0a9f02c146926b08c9f062308ad16995a718
Ivan Zulyan Pratama <zulyanivan11@gmail.com>
2022-12-02 00:55:14 +0700

    unitTest

---
+++ app/src/test/java/com/ace/c11flight/ui/view/LoginActivityTest.kt
@@ -0,0 +1,20 @@
+package com.ace.c11flight.ui.view
+
+import org.junit.Assert.*
+
+import org.junit.Test
+
+class LoginActivityTest {
+
+    @Test
+    fun onCreate() {
+    }
+
+    @Test
+    fun saveLoginInfo() {
+    }
+
+    @Test
+    fun onBackPressed() {
+    }
+}
